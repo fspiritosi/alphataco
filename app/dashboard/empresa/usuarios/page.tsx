f806253803119964cf6e71a307211bac71445801
fspiritosi <fspiritosi@gmail.com>
2025-09-24 11:04:11 -0300

    Implementación de Sidebar y Breadcrumb / Carpeta Modulos y Carpeta Shared

---
+++ app/dashboard/empresa/usuarios/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return (
+      <div>Mis usuarios</div>
+    )
+  }
+  
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import { AppSidebar } from "@/shared/features/sidebar/compnentes/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function Page() {
+  return (
+    <div>hola</div>
+  )
+}
